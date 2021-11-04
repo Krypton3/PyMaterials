9f73a894e923c8699029fda35bc9bfe3a2e88aba
mhasan <mhasan@sandvine.com>
2021-11-04 16:22:28 -0400

    First Module Update

---
+++ DataExtraction/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template
+from MarvelExtraction import extract
+import json
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    extract()
+    return render_template('index.html')
+
+@app.route('/widgets')
+def get_widgets() :
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1",
+    database="inventory"
+  )
+  cursor = mydb.cursor()
+
+
+  cursor.execute("SELECT * FROM widgets")
+
+  row_headers=[x[0] for x in cursor.description] #this will extract row headers
+
+  results = cursor.fetchall()
+  json_data=[]
+  for result in results:
+    json_data.append(dict(zip(row_headers,result)))
+
+  cursor.close()
+
+  return json.dumps(json_data)
+
+@app.route('/initdb')
+def db_init():
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1"
+  )
+  cursor = mydb.cursor()
+
+  cursor.execute("DROP DATABASE IF EXISTS inventory")
+  cursor.execute("CREATE DATABASE inventory")
+  cursor.close()
+
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1",
+    database="inventory"
+  )
+  cursor = mydb.cursor()
+
+  cursor.execute("DROP TABLE IF EXISTS widgets")
+  cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+  cursor.close()
+
+  return 'init database'
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# jPy
+# jPy is a public repo to present different learning materials using python
+
+# Module #1: Data Extraction
+
+1. This module is based on Docker, Python, Flask, BeautifulSoup and requests
