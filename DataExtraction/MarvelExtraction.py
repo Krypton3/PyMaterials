5c57260ae82ce855f076cc7daada849bf671762e
H M Mahedi Hasan <jisan2723@gmail.com>
2024-09-03 18:21:06 -0400

    Modification of data extraction codebase

--- DataExtraction/MarvelExtraction.py
+++ DataExtraction/MarvelExtraction.py
@@ -1,70 +1,59 @@
-from bs4 import BeautifulSoup
-import requests
-import json
 import csv
+import json
+import requests
+from bs4 import BeautifulSoup
 
 
 def extract():
     id = ['0371746', '0800080', '1228705', '0800369', '0458339']
 
-    # id = ['5095030']
+    # Headers: A dictionary of headers like content-type, date, server, etc.
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+
     with open('marvel.csv', mode='w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['movie_name', 'movie_year', 'movie_rating_type', 'movie_description', 'movie_keywords',
                          'movie_genre', 'movie_actors', 'movie_rating_user', 'movie_rating', 'direction', 'writers',
                          'storyline'])
 
-        track = 0
-        while track < len(id):
-            page_link = "http://www.imdb.com/title/tt" + str(id[track])
-            page_response = requests.get(page_link, timeout=5)
+        for movie_ids in id:
+            # base url
+            page_link = "https://www.imdb.com/title/tt" + movie_ids + "/"
+            # request to get html/json content of the base url with a code
+            page_response = requests.get(page_link, headers=headers, timeout=5)
+            if page_response.status_code == 403:
+                print(f"Access denied to {page_link}.")
+                continue
+
+            # BeautifulSoup to extract data from HTML
             page_content = BeautifulSoup(page_response.content, "html.parser")
-            data = json.loads(page_content.find('script', type='application/ld+json').text)
+            script_tag = page_content.find('script', type='application/ld+json')
 
-            get = page_content.find('span', attrs={"class": "TitleBlockMetaData__ListItemText-sc-12ein40-2 jedhex"})
-            movie_year = get.text
+            if script_tag is None:
+                print(f"No ld+json script tag found for {page_link}")
+                continue
+            data = json.loads(script_tag.string)
 
+            # fetching basic movie informations
             movie_name = data['name']
-            movie_rating_type = data['contentRating']
+            movie_year = data['datePublished']
+            movie_rating_type = data.get('contentRating', 'N/A')
             movie_description = data['description']
-            movie_keywords = data['keywords']
-            movie_genre = data['genre']
-            movie_actors = len(data['actor'])
+            movie_keywords = data.get('keywords', 'N/A')
+            movie_genre = data.get('genre', 'N/A')
             movie_rating_user = data['aggregateRating']['ratingCount']
             movie_rating = data['aggregateRating']['ratingValue']
 
-            i = 0
-            stars = []
-            while i < movie_actors:
-                stars.append(data['actor'][i]['name'])
-                i = i + 1
+            # fetching stars, direction, and writers
+            stars = [actor['name'] for actor in data['actor']]
+            direction = [director['name'] for director in data['director']] if isinstance(data['director'], list) else [data['director']['name']]
+            writers = [creator['name'] for creator in data['creator'] if 'name' in creator]
 
-            direction = []
-            if isinstance(data['director']) == list:
-                dict = len(data['director'])
-                i = 0
-                while i < dict:
-                    if 'name' in data['director'][i]:
-                        direction.append(data['director'][i]['name'])
-                    i = i + 1
-            else:
-                direction.append(data['director']['name'])
-
-            writers = []
-            if isinstance(data['creator']) == list:
-                dict = len(data['creator'])
-                i = 0
-                while i < dict:
-                    if 'name' in data['creator'][i]:
-                        writers.append(data['creator'][i]['name'])
-                    i = i + 1
-            else:
-                writers.append(data['creator']['name'])
-
+            # fetching storyline with specific div and class
             get = page_content.find('div', attrs={"class": "ipc-html-content ipc-html-content--base"})
-            storyline = get.text
+            storyline = get.text if get else 'N/A'
 
             writer.writerow([movie_name, movie_year, movie_rating_type, movie_description, movie_keywords, movie_genre,
                             stars, movie_rating_user, movie_rating, direction, writers, storyline])
-
-            track = track + 1
